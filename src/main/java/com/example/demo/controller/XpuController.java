9a2102332c834cc57c45ee095de3e687acbfc3cb
sasca37 <sasca37@naver.com>
2021-07-19 07:47:20 +0900

    156

--- src/main/java/com/example/demo/controller/XpuController.java
+++ src/main/java/com/example/demo/controller/XpuController.java
@@ -2,13 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.XpuDto;
 import com.example.demo.service.XpuService;
+import com.google.gson.Gson;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @Log4j2
@@ -21,9 +25,32 @@ public class XpuController {
     public String test(Model model) throws Exception {
         List<XpuDto> list = xpuService.boardList();
         log.info("aaa"+list);
-
         model.addAttribute("list",list);
-//        log.info(list.);
         return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/test")
+    public List<XpuDto> test2(Model model) throws Exception {
+        List<XpuDto> list = xpuService.boardList();
+        log.info("aaa"+list);
+        model.addAttribute("list",list);
+//        log.info(list.); , @RequestParam String list
+        return list;
+    }
+
+    @GetMapping("/ajax.do")
+    @ResponseBody
+    public void ajax(HttpServletResponse response, List<XpuDto> list) throws Exception {
+        log.info("호출완");
+        Gson gson = new Gson();
+
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("rs", list);
+        data.put("msg", "json data");
+        data.put("name", "ljbjh");
+
+        response.getWriter().print(gson.toJson(data));
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -7,13 +7,32 @@
 <head>
 </head>
 <body>
-<script type="text/javascript">
-    var list = '<c:forEach items="${list.type}" var="list"/>';
-    console.log(list);
+<%--<script type="text/javascript">--%>
+<%--    var list = '<c:out value="${list[0]}"/>';--%>
+<%--    console.log(list);--%>
 
 
+<%--</script>--%>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+    const list = '<c:out value="${list}"/>';
+    $(function () {
+        $.ajax({
+            url: "/ajax.do", // 호출할 주소
+            data: {
+                name : list
+                <%--type: '<c:out value="${list[0].type}"/>',--%>
+                <%--no: '<c:out value="${list[0].no}"/>',--%>
+                <%--per: '<c:out value="${list[0].per}"/>'--%>
+            }, // 넘길 데이터
+            dataType: "json", // 데이터 타입 json으로 설정 <- 이걸 안하면 밑에 처럼 JSON.parse를 해야함
+            success: function(data) { // 결과 받기
+                //data = JSON.parse(data); // JSON 형태로 파싱
+                console.log(data);
+            }
+        });
+    });
 </script>
-
 <div class="container">
     <br>
     <h2>조회 목록</h2>
